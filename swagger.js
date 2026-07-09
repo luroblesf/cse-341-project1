3d6c7d77d87d535724590112240190c0bddb5225
luroblesf <rob23047@byui.edu>
2026-07-09 11:06:46 -0600

    changing host

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'CSE 341 Project 1 API',
         description: 'API documentation for CSE 341 Project 1'
     },
-    host: 'localhost:3000',
+    host: 'cse-341-project1-lahm.onrender.com',
     schemes: ['http', 'https'],
 };
 
